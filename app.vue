6d15b973d3959691136f1d194e31cf463c9deca7
Apidet Keodpakprak <apidetkeodpakprak@gmail.com>
2023-01-28 23:19:54 +0700

    first commit

---
+++ app.vue
@@ -0,0 +1,24 @@
+<!-- <script lang="ts" setup>
+  useHead({
+    title: 'Home',
+    meta: [
+      { 
+        name: 'description', 
+        content: 'Home Nuxt 3, IT Genius Engineering' 
+      },
+      {
+        name: 'keywords',
+        content: 'Home, Nuxt 3, Learning Nuxt 3'
+      },
+    ],
+  })
+</script> -->
+<template>
+  <head>
+<link href="https://fonts.googleapis.com/css2?family=Kanit:ital,wght@0,400;1,300;1,500&display=swap" rel="stylesheet">
+
+  </head>
+ <NuxtLayout>  <!--defualt -->
+  <NuxtPage/>  <!--page index -->
+ </NuxtLayout>
+</template>
---
+++ layouts/default.vue
@@ -0,0 +1,18 @@
+<script lang="ts" setup>
+import Header from '~~/components/App/Header.vue';
+
+</script>
+
+<template>
+
+<div>
+  <AppHeader/>
+  <div class="px-4 mx-auto sm:px-0">
+    <slot />
+  </div>
+ 
+  <AppFooter/>
+</div>
+</template>
+
+<style scoped></style>
